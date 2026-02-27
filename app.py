128cd8aed401cc88911de810440761400927892e
Binoy <haldarbinoy257@gmail.com>
2026-02-28 04:38:15 +0530

    fix render port binding

--- app.py
+++ app.py
@@ -97,4 +97,5 @@ def contact():
 
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
